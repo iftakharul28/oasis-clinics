f41baac77316c207c3174e121137e290270acc30
iftakharul28 <iftakharul28@gmail.com>
2022-04-22 01:36:20 +0600

    next j config file rewrited

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 const withSourcebit = require('sourcebit').sourcebitNext();
-const withOptimizedImages = require('next-optimized-images');
+
 module.exports = withSourcebit({});
-module.exports = withOptimizedImages({
-  optimizeImagesInDev: true,
-});
